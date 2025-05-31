9ea690fbcae14883c0027c2166932ff12fb8f0ae
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-06-01 05:23:53 +0800

    Semi-set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<>
-			<h1>Hello World</h1>
+			<Button>Hello World!</Button>
 		</>
 	);
 }
