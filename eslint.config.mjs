c703ded3cfcce155c2afbd594bf01a3f156d36d0
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-06-01 12:47:00 +0800

    finish setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    ignorePatterns: ["src/generated/**/*.**"],
+  }),
+];
 
 export default eslintConfig;
