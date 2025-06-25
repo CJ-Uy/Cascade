20828d47c8523c9a67098783089d706438fecc32
Remy <75460325+CJ-Uy@users.noreply.github.com>
2025-06-25 21:15:17 +0800

    added new database model

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,8 @@
 import { createAuthClient } from "better-auth/react";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const authClient = createAuthClient({
 	baseURL: process.env.NEXT_PUBLIC_AUTH_API_URL,
 });
