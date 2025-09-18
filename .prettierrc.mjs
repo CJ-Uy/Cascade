0cb61135b007d8b70e0070136578b4be2798493b
Remy <75460325+CJ-Uy@users.noreply.github.com>
2025-09-18 22:48:42 +0800

    re-init

--- .prettierrc.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-	useTabs: true,
-	semi: true,
-	trailingComma: "all",
-	doubleQuote: true,
-	printWidth: 100,
-	plugins: ["prettier-plugin-tailwindcss"], // Array of strings is fine here
-};
--- src/store/useAuthStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useAuthStore = create(() => ({
-	userId: "",
-	authenticated: false,
-	setNewValue: (newId) => set({ userId: newId, authenticated: true }),
-}));
