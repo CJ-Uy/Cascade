63050cc8abcba4f049e041c8f6cfecfcf05a0849
Remy <75460325+CJ-Uy@users.noreply.github.com>
2025-10-03 18:50:53 +0800

    added create requisition ui

---
+++ components/requisitions/create/FormSelector.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Label } from '@/components/ui/label';
+import { type Form } from '@/components/management/forms/FormList';
+
+interface FormSelectorProps {
+  availableForms: Form[];
+  selectedFormId: string | undefined;
+  onSelectForm: (formId: string) => void;
+}
+
+export function FormSelector({ availableForms, selectedFormId, onSelectForm }: FormSelectorProps) {
+  return (
+    <div className="space-y-2">
+      <Label htmlFor="form-select">Select a Form</Label>
+      <Select value={selectedFormId} onValueChange={onSelectForm}>
+        <SelectTrigger className="w-[300px]">
+          <SelectValue placeholder="Choose a form to fill out" />
+        </SelectTrigger>
+        <SelectContent>
+          {availableForms.map(form => (
+            <SelectItem key={form.id} value={form.id}>
+              {form.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
